80be1ad34f212ff0112957d241ae604688343680
Khursheed Ahmed <118465990+Pro-Programmer38@users.noreply.github.com>
2023-12-05 22:05:41 +0500

    Update Linked_List.cpp

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -24,7 +24,7 @@ void LinkedList::insertAtTail(int val) {
          head = newNode;
          count++;
          
-         cout<<"\nThe node is inserted successfully.";
+//         cout<<"\nThe node is inserted successfully.";
      }
      else {
          Node* temp = head;
@@ -34,7 +34,7 @@ void LinkedList::insertAtTail(int val) {
          temp->setNext(newNode);
          count++;
          
-         cout<<"\nThe node is inserted succussfully.";
+//         cout<<"\nThe node is inserted succussfully.";
      }
 }
 
@@ -196,4 +196,97 @@ int LinkedList::searchMiddle() {
 
 }
 
+void LinkedList::removeMiddle() {
+    if(isEmpty()) {
+        cout<<"\nThe list is empty.";
+        return;
+    }
+
+    Node* slowPtr = head;
+    Node* fastPtr = head;
+    Node* prev = nullptr;
+
+    while (fastPtr != nullptr && fastPtr->getNext() != nullptr) {
+        fastPtr = fastPtr->getNext()->getNext();
+        prev = slowPtr;
+        slowPtr = slowPtr->getNext();
+    }
+
+    prev->setNext(slowPtr->getNext());
+    delete slowPtr;
+    cout << "Middle node deleted successfully.";
+}
+
+
+void LinkedList::printReverse(Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+    printReverse(node->getNext());
+    cout << node->getData() << " ";
+}
+
+// Call this function from outside to start printing the list in reverse
+void LinkedList::printReverseList() {
+    cout << "Reversed Linked List: ";
+    printReverse(head);
+    cout << endl;
+}
+
+
+void LinkedList::removeDuplicates() {
+    Node* current = head;
+
+    while (current != nullptr && current->getNext() != nullptr) {
+        if (current->getData() == current->getNext()->getData()) {
+            Node* temp = current->getNext();
+            current->setNext(temp->getNext());
+            delete temp;
+        } else {
+            current = current->getNext();
+        }
+    }
+
+    cout << "Duplicates removed successfully." <<endl;
+}
+
+void LinkedList::segregateEvenOdd() {
+    if (head == nullptr || head->getNext() == nullptr) {
+        cout << "List is empty or has only one node." << endl;
+        return;
+    }
+
+    LinkedList evenList;
+    LinkedList oddList;
+
+    Node* current = head;
+
+    while (current != nullptr) {
+        if (current->getData() % 2 == 0) {
+            evenList.insertAtTail(current->getData());
+        } else {
+            oddList.insertAtTail(current->getData());
+        }
+        current = current->getNext();
+    }
+
+    cout << "\nEven elements list: ";
+    evenList.printAll();
+
+    cout << "\nOdd elements list: ";
+    oddList.printAll();
+}
+
+LinkedList LinkedList::mergeLists(LinkedList list1, LinkedList list2) {
+    LinkedList mergedList = list1; 
+
+    Node* curr = list2.head;
+
+    while (curr != nullptr) {
+        mergedList.insertAtTail(curr->getData());
+        curr = curr->getNext();
+    }
+
+    return mergedList;
+}
 
